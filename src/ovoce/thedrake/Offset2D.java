ad2280622c46ac201b5e7fa43803b2787f34ee9a
krizj <josef.kriz@jumpshot.com>
2017-10-18 09:40:08 +0200

    cviceni2-2

--- src/ovoce/thedrake/Offset2D.java
+++ src/ovoce/thedrake/Offset2D.java
@@ -5,11 +5,6 @@ public class Offset2D {
     public final int x;
     public final int y;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Offset2D test = new Offset2D(1, 2);
-    }
-
     public Offset2D(int x, int y) {
         this.x = x;
         this.y = y;
