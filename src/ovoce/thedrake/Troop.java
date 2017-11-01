5a0596ff03170f652db01342a830cf6db4b8b5a2
Lenka Obermajerová <Lenka.Obermajerova@seznam.cz>
2017-11-01 10:44:39 +0100

    ChangesFrom v Troop

--- src/ovoce/thedrake/Troop.java
+++ src/ovoce/thedrake/Troop.java
@@ -1,6 +1,9 @@
 //třída představuje jednotku ve hře
 package ovoce.thedrake;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Troop {
     private final TroopInfo info;
     private final PlayingSide side;
@@ -11,7 +14,21 @@ public class Troop {
         this.side = side;
         this.face = face;
     }
+    // Vytvoří jednotku lícem nahoru
+    public Troop(TroopInfo info, PlayingSide side) {
+        this.info = info;
+        this.side = side;
+        this.face = TroopFace.BACK;
+    }
 
+    // Všechny změny desky, které může jednotka provést na desce board, pokud stojí na pozici pos.
+    public List<BoardChange> changesFrom(TilePosition pos, Board board) {
+        List<BoardChange> changes= new ArrayList<>();
+        for(TroopAction action: info.actions(face))
+            changes.addAll(action.changesFrom(pos,side,board));
+        return changes;
+    }
+    
     public TroopInfo info() {
         return this.info;
     }
