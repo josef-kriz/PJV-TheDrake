76dd083a501547bc6a92c1139c24878122eb0378
krizj <josef.kriz@jumpshot.com>
2017-12-06 10:07:10 +0100

    ukol6 - upravy aby to vypadalo ze jsme si dali hoodne zalezet

--- src/ovoce/thedrake/view/MenuController.java
+++ src/ovoce/thedrake/view/MenuController.java
@@ -8,30 +8,41 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 public class MenuController implements Initializable {
-//    @FXML private Label label;
-//    @FXML private ListView<String> listView;
+    @FXML private Label label;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-//        label.setText("Ahoj");
-//        listView.getItems().add("Petr");
-//        listView.getItems().add("Pavel");
-//        listView.getItems().add("Ivan");
-//        listView.getItems().add("Jana");
-//        listView.getItems().add("Eva");
-//
-//        listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-//            @Override
-//            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-//                label.setText(newValue);
-//            }
-//        });
+
     }
 
     public void exit(ActionEvent event) {
         System.exit(0);
+    }
+
+    public void showMultiplayer(MouseEvent event) {
+        setLabel("Hra dvou hráčů");
+    }
+
+    public void showComputer(MouseEvent event) {
+        setLabel("Hra proti počítači");
+    }
+
+    public void showInternet(MouseEvent event) {
+        setLabel("Hra na internetu");
+    }
+
+    public void showExit(MouseEvent event) {
+        setLabel("Konec");
+    }
+
+    public void hideLabel(MouseEvent event) {
+        setLabel("");
+    }
+
+    private void setLabel(String text) {
+        label.setText(text);
     }
 }
